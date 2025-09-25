3bf002ca3aea25591527346c43723b4d1529b762
Jordan Stewart <{ID}+{username}@users.noreply.github.com>
2025-09-24 20:38:56 -0400

    A few examples of using let, const, and basic math

--- javascriptbasics.js
+++ javascriptbasics.js
@@ -16,6 +16,11 @@ console.log(age);
 
 // Using const to keep a variable constant
 const pi = 3.14;
-pi = 10;
+// pi = 10;
+
 // This throws a TypeError since pi is a constant
 console.log(pi);
+
+// Working with numbers
+console.log((3 + 2) - 76 * (1 + 1))
+
