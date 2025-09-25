420cd3dacb7d93041cf7c376b0b741a053aa4235
Jordan Stewart <{ID}+{username}@users.noreply.github.com>
2025-09-25 19:38:46 -0400

    Javascript.info variables tasks complete.

---
+++ js-info.js
@@ -0,0 +1,18 @@
+    // Below are tasks found on the website javascript.info part of TOP curriculum.
+    
+    // WORKING WITH VARIABLES
+    // 1. Declare two variables: admin and name.
+    let admin;
+    let name;
+    // 2. Assign the value "John" to name.
+    name = "John";
+    // 3. Copy the value from name to admin.
+    admin = name;
+    // 4. Show the value of admin using alert (must output “John”).
+    alert(admin);
+
+    // GIVING THE RIGHT NAME
+    // 1. Create a variable with the name of our planet. How would you name such a variable?
+    let homePlanet = "Earth";
+    // 2.Create a variable to store the name of a current visitor to a website. How would you name that variable?
+    let currentUser = "John";
