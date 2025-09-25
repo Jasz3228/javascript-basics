4b9a34826a7fc0b7d3c1db23ad8d25a51042ad3e
Jordan Stewart <{ID}+{username}@users.noreply.github.com>
2025-09-24 20:24:38 -0400

    Changed file names

---
+++ javascriptbasics.js
@@ -0,0 +1,21 @@
+console.log("Hello, World!")
+
+// Declaring variables using let
+let first = "John";
+let last = "Doe";
+
+console.log(first);
+console.log(last);
+
+// Re-assigning variables
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+// Using const to keep a variable constant
+const pi = 3.14;
+pi = 10;
+// This throws a TypeError since pi is a constant
+console.log(pi);
