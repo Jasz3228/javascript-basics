3d9576e20a718235e971b3ec799ec707387307f0
Jordan Stewart <{ID}+{username}@users.noreply.github.com>
2025-09-29 10:45:46 -0400

    More tasks completed on operators.

--- js-info.js
+++ js-info.js
@@ -16,3 +16,52 @@
     let homePlanet = "Earth";
     // 2.Create a variable to store the name of a current visitor to a website. How would you name that variable?
     let currentUser = "John";
+
+    // OPERATOR TASKS
+    
+    // The postfix and prefix forms
+    // What are the final values of all variables a, b, c and d after the code below?
+        // let a = 1, b = 1;
+
+        // let c = ++a; // 2 prefix returns new value
+        // let d = b++; // 1 postfix returns old value
+
+    //What are the values of a and x after the code below?
+
+    // let a = 2;
+
+    // let x = 1 + (a *= 2);
+        a = 4;
+        x = 5;
+
+    // What are results of these expressions?
+        // "" + 1 + 0 // "10"
+        // "" - 1 + 0 // -1
+        // true + false // 1
+        // 6 / "3" // 2
+        // "2" * "3" // 6
+        // 4 + 5 + "px" "9px"
+        // "$" + 4 + 5 "$45"
+        // "4" - 2 // 2
+        // "4px" - 2 // NaN
+        // "  -9  " + 5 // "  -9  5"
+        // "  -9  " - 5 // -14
+        // null + 1 // 1
+        // undefined + 1 // Nan
+        // " \t \n" - 2 // -2
+
+    // Here’s a code that asks the user for two numbers and shows their sum.
+
+    // It works incorrectly. The output in the example below is 12 (for default prompt values).
+
+    // Why? Fix it. The result should be 3.
+
+        // let a = prompt("First number?", 1);
+        // let b = prompt("Second number?", 2);
+
+        // alert(a + b); // 12
+
+        let a1 = prompt("First number?", 1);
+        let b2 = prompt("Second number?", 2);
+
+        console.log(+a1 + +b2);
