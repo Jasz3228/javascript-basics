1872acef5e9b104de4c567484acbf948375e8434
Jordan Stewart <{ID}+{username}@users.noreply.github.com>
2025-10-10 21:32:33 -0400

    "Commented out old tasks."

--- js-info.js
+++ js-info.js
@@ -1,98 +1,363 @@
-    // Below are tasks found on the website javascript.info part of TOP curriculum.
+// Below are tasks found on the website javascript.info part of TOP curriculum.
+
+// WORKING WITH VARIABLES
+// 1. Declare two variables: admin and name.
+// let admin;
+// let name;
+// 2. Assign the value "John" to name.
+// name3 = "John";
+// 3. Copy the value from name to admin.
+// admin = name3;
+// 4. Show the value of admin using alert (must output “John”).
+
+// alert(admin);
+
+// GIVING THE RIGHT NAME
+//  1. Create a variable with the name of our planet. How would you name such a variable?
+// let homePlanet = "Earth";
+//  2.Create a variable to store the name of a current visitor to a website. How would you name that variable?
+// let currentUser = "John";
+
+// OPERATOR TASKS
+
+// The postfix and prefix forms
+// What are the final values of all variables a, b, c and d after the code below?
+    // let a = 1, b = 1;
+
+    // let c = ++a; // 2 prefix returns new value
+    // let d = b++; // 1 postfix returns old value
+
+//What are the values of a and x after the code below?
+
+// let a = 2;
+
+// let x = 1 + (a *= 2);
+//     a = 4;
+//     x = 5;
+
+// // What are results of these expressions?
     
-    // WORKING WITH VARIABLES
-    // 1. Declare two variables: admin and name.
-    let admin;
-    let name;
-    // 2. Assign the value "John" to name.
-    name = "John";
-    // 3. Copy the value from name to admin.
-    admin = name;
-    // 4. Show the value of admin using alert (must output “John”).
-    alert(admin);
+//     "" + 1 + 0 // "10"
+//     "" - 1 + 0 // -1
+//     true + false // 1
+//     6 / "3" // 2
+//     "2" * "3" // 6
+//     4 + 5 + "px" // "9px"
+//     "$" + 4 + 5 // "$45"
+//     "4" - 2 // 2
+//     "4px" - 2 // NaN
+//     "  -9  " + 5 // "  -9  5"
+//     "  -9  " - 5 // -14
+//     null + 1 // 1
+//     undefined + 1 // Nan
+//     " \t \n" - 2 // -2
 
-    // GIVING THE RIGHT NAME
-    // 1. Create a variable with the name of our planet. How would you name such a variable?
-    let homePlanet = "Earth";
-    // 2.Create a variable to store the name of a current visitor to a website. How would you name that variable?
-    let currentUser = "John";
+// Here’s a code that asks the user for two numbers and shows their sum.
 
-    // OPERATOR TASKS
+// It works incorrectly. The output in the example below is 12 (for default prompt values).
+
+// Why? Fix it. The result should be 3.
+
+    // let a = prompt("First number?", 1);
+    // let b = prompt("Second number?", 2);
+
+    // alert(a + b); // 12
+
+    // let a1 = prompt("First number?", 1);
+    // let b2 = prompt("Second number?", 2);
+
+    //console.log(+a1 + +b2);
+
+// Data types task
+
+    // What is output of the script?
+
+    // let name2 = "Ilya";
+
+    // alert( `hello ${1}` ); // hello 1
+
+    // alert( `hello ${"name"}` ); // hello name
+
+    // alert( `hello ${name2}` ); // ? hello Ilya
+
+// Comparisons
+
+    //What will be the result for these expressions?
+
+    // 5 > 4 // true
+
+    // "apple" > "pineapple" // false
+
+    // "2" > "12" // true
+
+    // undefined == null // true
+
+    // undefined === null // false
+
+    // null == "\n0\n" // false
+
+    // null === +"\n0\n" // false
+
+// Logical operators
+
+    // Whats the result of OR?
+
+    // alert( null || 2 || undefined ); // 2
+
+    // What's the result of OR'ed alerts?
+
+    // alert( alert(1) || 2 || alert(3) ); // 1, 2
+
+    // What is the result of AND?
     
-    // The postfix and prefix forms
-    // What are the final values of all variables a, b, c and d after the code below?
-        // let a = 1, b = 1;
+    // alert( 1 && null && 2 ); // null
 
-        // let c = ++a; // 2 prefix returns new value
-        // let d = b++; // 1 postfix returns old value
+    // What is the result of AND'ed alerts?
 
-    //What are the values of a and x after the code below?
+    // alert( alert(1) && alert(2) ); // 1 then undefined
 
-    // let a = 2;
+    // THe result of OR AND OR
 
-    // let x = 1 + (a *= 2);
-        a = 4;
-        x = 5;
+    // alert( null || 2 && 3 || 4 ); // 3
 
-    // What are results of these expressions?
-        // "" + 1 + 0 // "10"
-        // "" - 1 + 0 // -1
-        // true + false // 1
-        // 6 / "3" // 2
-        // "2" * "3" // 6
-        // 4 + 5 + "px" "9px"
-        // "$" + 4 + 5 "$45"
-        // "4" - 2 // 2
-        // "4px" - 2 // NaN
-        // "  -9  " + 5 // "  -9  5"
-        // "  -9  " - 5 // -14
-        // null + 1 // 1
-        // undefined + 1 // Nan
-        // " \t \n" - 2 // -2
-
-    // Here’s a code that asks the user for two numbers and shows their sum.
-
-    // It works incorrectly. The output in the example below is 12 (for default prompt values).
-
-    // Why? Fix it. The result should be 3.
-
-        // let a = prompt("First number?", 1);
-        // let b = prompt("Second number?", 2);
-
-        // alert(a + b); // 12
-
-        let a1 = prompt("First number?", 1);
-        let b2 = prompt("Second number?", 2);
-
-        console.log(+a1 + +b2);
-
-    // Data types task
-
-        // What is output of the script?
-
-        let name2 = "Ilya";
-
-        alert( `hello ${1}` ); // hello 1
-
-        alert( `hello ${"name"}` ); // hello name
-
-        alert( `hello ${name2}` ); // ? hello Ilya
     
-    // Comparisons
+    // Check the range between
 
-        //What will be the result for these expressions?
+    /* Write an if condition to check that age is between 14 and 90 inclusively.
+    “Inclusively” means that age can reach the edges 14 or 90.*/
+    
+    // let age = 90;
 
-        5 > 4 // true
+    // if (age >= 14 && age <= 90) {
+    //     // alert(`Your ${age} years old!`);
+    // }
 
-        "apple" > "pineapple" // false
+    // Check the range outside
 
-        "2" > "12" // true
+    // Write an if condition to check that age is NOT between 14 and 90 inclusively. Create two variants: the first one using NOT !, the second one – without it.
 
-        undefined == null // true
+    // if (age < 14 || age > 90);
 
-        undefined === null // false
+    // if (!(age >= 14 && age <= 90));
 
-        null == "\n0\n" // false
+    // A question about "if"
 
-        null === +"\n0\n" // false
+    // Which of these alerts are going to execute?
+    // What will the results of the expressions be inside if(...)?
 
+    // if (-1 || 0) alert( 'first' ); // Runs, -1 truthy
+    // if (-1 && 0) alert( 'second' ); // Doesn't run
+    // if (null || -1 && 1) alert( 'third' ); //  runs, 1
+
+
+    // Check the login
+
+     /* Write the code which asks for a login with prompt.
+
+     If the visitor enters "Admin", then prompt for a password, if the input is an empty line or Esc – show “Canceled”, if it’s another string – then show “I don’t know you”
+     The password is checked as follows
+     If it equals “TheMaster”, then show “Welcome!”,
+     Another string – show “Wrong password”,
+     For an empty string or cancelled input, show “Canceled"*/
+
+    // let userName = // prompt("Who's there?");
+    
+    
+    
+    // if (userName === "Admin") {
+    //     let pass = // prompt("Password?");
+
+    //     if (pass === "TheMaster") {
+    //         // alert("Welcome!");
+    //     } else if (pass === "" || pass === null) {
+    //        // alert("Canceled");
+    //     } else {
+    //         // alert("Wrong password");
+    //     }
+    
+    // }   else if (userName === "" || userName === null) {
+    //     // alert("Canceled");
+    // }   else {
+    //     // alert( "I don't know you");
+    // }
+
+// Conditional branching: if, "?"
+
+    // Will alert be shown?
+        // if ("0") {
+        //   // alert( 'Hello' );
+        // } // Yes Any string except an empty one becomes true
+        
+    // The name of JavaScript
+
+    //Using the if..else construct, write the code which asks: ‘What is the “official” name of JavaScript?’
+        
+    // If the visitor enters “ECMAScript”, then output “Right!”, otherwise – output: “You don’t know? ECMAScript!”
+        
+    // let officialName = // prompt("What is the official Java script name?", "");
+
+        // if (officialName === "ECMAScript") {
+        //     // alert("Right!");
+        // } else {
+        //     // alert("You don't know? ECMAScript!");
+        // }
+
+    // Show the sign
+
+        // Using if..else, write the code which gets a number via prompt and then shows in alert:
+
+        // 1, if the value is greater than zero,
+        // -1, if less than zero,
+        // 0, if equals zero.
+
+    // let num = Number(prompt("Enter a number", ""));
+
+        // if (num > 0) {
+        //     alert(1);
+        // } else if (num < 0) {
+        //     alert(-1);
+        // } else {
+        //     alert(0);
+        // }
+
+    // Rewrite this if using the conditional operator "?"
+
+        // let result = (a + b < 4) ? "below" : "Over";
+
+    // Rewrite if...else using multiple ternary operators "?"
+    
+        // let message = (login == "Employee") ? "Hello" : 
+        //     (login == "Director") ? "Greetings" :
+        //     (login == "") ? "No login" : 
+        //     "";
+        
+
+// The switch Statement
+
+    //Rewrite the "switch" into an "if"
+
+    /*switch (browser) {
+        case 'Edge':
+            alert( "You've got the Edge!" );
+            break;
+
+        case 'Chrome':
+        case 'Firefox':
+        case 'Safari':
+        case 'Opera':
+            alert( 'Okay we support these browsers too' );
+            break;
+
+        default:
+            alert( 'We hope that this page looks ok!' );
+    }*/
+
+    // let browser = prompt("Enter a browser.");
+
+    // if (browser === "Edge") {
+    //     alert("You've got the Edge!");
+    // } else if (browser === "Chrome" 
+    //     || browser === "Firefox" 
+    //     || browser === "Safari" 
+    //     || browser === "Opera") {
+    //         alert("Okay we support these browsers too" );
+    //     } else {
+    //         alert("We hope that this page looks ok!" );
+    //     }
+
+
+    //Rewrite "if" into "switch"
+
+        /*let a = +prompt('a?', '');
+
+        if (a == 0) {
+            alert( 0 );
+        }
+        if (a == 1) {
+            alert( 1 );
+        }
+
+        if (a == 2 || a == 3) {
+            alert( '2,3' );
+        }*/
+
+        // let a = +prompt("a?", "");
+
+        // switch (a) {
+        //     case 0:
+        //         alert(0);
+        //         break;
+        //     case 1:
+        //         alert(1);
+        //         break;
+        //     case 2:
+        //     case 3:
+        //         alert("2,3");
+        //         break;
+        // }
+
+
+// Functions
+    // Rewrite the function using "?" or "||"
+        /*function checkAge(age) {
+            if (age > 18) {
+                return true;
+            } else {
+                return confirm('Did parents allow you?');
+            }
+        }*/
+
+        // function checkAge(age) {
+        //     return (age > 18) ? true : confirm("Did parents allow you?");
+        // }
+
+        // function checkAge(age) {
+        //     return (age > 18) || confirm("Did parents allow you?");
+        // }
+
+    // Write a function min(a,b) which returns the least of two numbers a and b.
+
+    // For instance:
+
+        // min(2, 5) == 2
+        // min(3, -1) == -1
+        // min(1, 1) == 1
+
+        // function min(a, b) {
+        //     if (a < b) {
+        //         return a;
+        //     } else {
+        //         return b;
+        //     }
+        // }
+        // console.log(min(2, 5));
+        // console.log(min(3, -1));
+        // console.log(min(1,1));
+        
+// Arrow function basics
+    // Rewrite with arrow functions
+    
+    /* function ask(question, yes, no) {
+            if (confirm(quesion)) yes();
+            else no();
+        }
+        
+        ask(
+        "Do you agree?",
+        function() {alert("You agreed.");},
+        function() {alert("You canceled the execution.");}
+        );
+    */
+
+
+        // function ask(question, yes, no) {
+        //     if (confirm(question)) yes();
+        //     else no();
+        // }
+
+        // ask(
+        //     "Do you agree?", 
+        //     () => alert("You canceled the execution."),
+        //     () => alert("You agreed.")
+        // );
+        
